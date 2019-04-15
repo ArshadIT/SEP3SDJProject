c70eccf3aebd1edde4516ed3bbb683ee038c40a2
ArshadIT <37140618+ArshadIT@users.noreply.github.com>
2019-04-15 15:02:51 +0200

    small change

---
+++ SEP3SDJProject/src/Domain/Hworld.java
@@ -0,0 +1,10 @@
+package Domain;
+
+public class Hworld {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello world");
+	}
+
+}
--- SEP3SDJProject/src/newpackage/HelloArshad.java
+++ SEP3SDJProject/src/newpackage/HelloArshad.java
@@ -4,6 +4,8 @@ public class HelloArshad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("hz is life");
 		System.out.println("hey, does this upload as it should?");
 
 	}
